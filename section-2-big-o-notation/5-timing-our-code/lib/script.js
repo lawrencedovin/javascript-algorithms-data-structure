c48d661fdcd83becfafaf30d1f15540c6c9fe03d
lawrencedovin <lawrence@qwikwire.com>
2020-08-06 20:22:10 -0400

    Added initial char count function.

---
+++ section-2-big-o-notation/5-timing-our-code/lib/script.js
@@ -0,0 +1,18 @@
+// function addUpTo(n) {
+//     let total = 0;
+//     for(let i = 1; i <= n; i++){
+//         total += i;
+//     }
+//     return total;
+// }
+
+// console.log(addUpTo(6));
+
+function addUpTo(n) {
+    return n * (n+1) / 2;
+}
+
+let t1 = performance.now();
+addUpTo(1000000000);
+let t2 = performance.now();
+console.log('Time Elapsed:' + (t2-t1)/1000 + ' seconds');
---
+++ section-4-problem-solving-approach/char-count/lib/script.js
@@ -0,0 +1,21 @@
+function charCount(str) {
+    // make object to return at end
+    let result = {};
+    // loop over string, per char
+    for(let i=0; i < str.length; i++){
+        //if char is a no/letter AND is a key in object, add 1 to count
+        let char = str[i].toLowerCase();
+        if (result[char] > 0) {
+            result[char]++;   
+        } 
+        //if char is a no/letter AND not in object, add it to object and set value to 1
+        else {
+            result[char] = 1;
+        }
+        //if char is something else (space, period, etc.) don't do anything
+    }
+    // return object at end
+    return result;
+}
+
+// if (typeof str[i] == "number" || typeof str[i] == "string")
