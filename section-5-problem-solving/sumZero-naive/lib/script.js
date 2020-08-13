7dab3ed0edd015fe67a370ed6a0b3ef95058fb25
lawrencedovin <lawrence@qwikwire.com>
2020-08-13 13:25:12 -0400

    Added another solution to sumZero naive.

--- section-5-problem-solving/sumZero-naive/lib/script.js
+++ section-5-problem-solving/sumZero-naive/lib/script.js
@@ -1,9 +1,23 @@
+// function sumZero(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         for(let j = arr.length-1; j > 0; j--) {
+//             if(arr[i] + arr[j] === 0) {
+//                 return [arr[i], arr[j]];
+//             }
+//         }
+//     }
+// }
+
 function sumZero(arr){
+    let positive = [];
+    let negative = [];
     for(let i = 0; i < arr.length; i++){
-        for(let j = arr.length-1; j > 0; j--) {
-            if(arr[i] + arr[j] === 0) {
-                return [arr[i], arr[j]];
-            }
+        arr[i] >= 0 ? positive.push(arr[i]) : negative.push(arr[i]);
+    }
+
+    for(let i = 0; i < arr.length; i++){
+        if(positive.includes(Math.abs(negative[i]))){
+            return [positive[i], negative[i]];
         }
     }
 }
