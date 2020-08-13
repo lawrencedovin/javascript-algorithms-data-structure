913fa6e588f623c4fcf8b35243f9ef1de154e7a6
lawrencedovin <lawrence@qwikwire.com>
2020-08-13 13:06:28 -0400

    Updated sumZero naive approach

--- section-5-problem-solving/sumZero-naive/lib/script.js
+++ section-5-problem-solving/sumZero-naive/lib/script.js
@@ -1,6 +1,6 @@
 function sumZero(arr){
     for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j < arr.length; j++) {
+        for(let j = arr.length-1; j > 0; j--) {
             if(arr[i] + arr[j] === 0) {
                 return [arr[i], arr[j]];
             }
