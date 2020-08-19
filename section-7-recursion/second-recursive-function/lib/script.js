0bd7367076d365ed5f1edd4ef96f9f5243b1f0ff
lawrencedovin <lawrence@qwikwire.com>
2020-08-19 16:45:26 -0400

    Added second recursive function.

---
+++ section-7-recursion/second-recursive-function/lib/script.js
@@ -0,0 +1,12 @@
+function sumRange(num) {
+
+    // if(num === 1) {
+    //     return 1;
+    // }
+
+    // return num + sumRange(num-1);
+
+    return num === 1 ? 1 : num + sumRange(num-1);
+}
+
+console.log(sumRange(4));
