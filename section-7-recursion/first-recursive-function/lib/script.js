2c304f015582bcb10129d68cc514cdca68459a12
lawrencedovin <lawrence@qwikwire.com>
2020-08-19 16:32:52 -0400

    Added recursive function notes.

--- section-7-recursion/first-recursive-function/lib/script.js
+++ section-7-recursion/first-recursive-function/lib/script.js
@@ -1,11 +1,18 @@
 function countDown(num) {
-    if(num <= 0){
-        console.log("All Done");
+    // 1. Make a Base Case which does a final return
+    if(num < 1) {
+        console.log("Countdown complete");
         return;
     }
+
+    // 2. Do an action if Base Case isn't met
+
     console.log(num);
     num--;
+
+    // 3. Call the recursive function to repeat the previous steps
+    // until Base Case is met
     countDown(num);
 }
 
-countDown(3);
+console.log(countDown(4));
