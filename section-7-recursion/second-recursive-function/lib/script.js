954dfc778f8378157567bed03cc017561cb50dff
lawrencedovin <lawrence@qwikwire.com>
2020-08-19 17:01:30 -0400

    Updated second recursive function notes.

--- section-7-recursion/second-recursive-function/lib/script.js
+++ section-7-recursion/second-recursive-function/lib/script.js
@@ -9,4 +9,12 @@ function sumRange(num) {
     return num === 1 ? 1 : num + sumRange(num-1);
 }
 
-console.log(sumRange(4));
+// sumRange(3)
+// return 3 + sumRange(2);
+// return 2 + sumRange(1);
+// return 1;
+
+// return 3 + sumRange(2);
+// return 2 + 1; -> 3
+
+// return 3 + 3 -> 6
