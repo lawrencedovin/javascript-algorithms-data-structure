17630cae10e0ab7ac6d4e707767a6e7e64b36767
lawrencedovin <lawrence@qwikwire.com>
2020-08-22 14:12:17 -0400

    Added binary search example.

---
+++ section-10-searching-algo/binary-search/lib/script.js
@@ -0,0 +1,46 @@
+// function binarySearch(arr, value) {
+//   // Create a left pointer at the start of the array.
+//   let left = 0;
+//   // Create a right pointer at the end of the array.
+//   let right = arr.length - 1;
+
+//   // Create a pointer in the middle.
+//   let middle = Math.floor((left + right) / 2);
+
+//   while (arr[middle] !== value && left <= right) {
+//     // if value is smaller than middle, move the left pointer up
+//     if (value < arr[middle]) {
+//       right = middle - 1;
+//     }
+//     // if value is bigger than middle, move right pointer down
+//     else {
+//       left = middle + 1;
+//     }
+//     middle = Math.floor((left + right) / 2);
+//   }
+
+//   if(arr[middle] == value) {
+//     return middle;
+//   } 
+//   return -1;
+// }
+
+function binarySearch(arr, value) {
+  // Create a left pointer at the start of the array.
+  let left = 0;
+  // Create a right pointer at the end of the array.
+  let right = arr.length - 1;
+  // Create a pointer in the middle.
+  let middle = Math.floor((left + right) / 2);
+
+  while (arr[middle] !== value && left <= right) {
+    // if value is smaller than middle, move the right pointer down
+    if (value < arr[middle]) right = middle - 1;
+    // if value is bigger than middle, move left pointer up
+    else left = middle + 1;
+    middle = Math.floor((left + right) / 2);
+  }
+
+  return arr[middle] == value ? middle : -1;
+}
+console.log(binarySearch([1, 2, 3, 4, 5], 1));
