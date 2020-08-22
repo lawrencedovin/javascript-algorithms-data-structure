de958107e350901ae95d4da798b38e093295ab00
lawrencedovin <lawrence@qwikwire.com>
2020-08-22 14:15:04 -0400

    Updated binary search notes.

--- section-10-searching-algo/binary-search/lib/script.js
+++ section-10-searching-algo/binary-search/lib/script.js
@@ -33,6 +33,8 @@ function binarySearch(arr, value) {
   // Create a pointer in the middle.
   let middle = Math.floor((left + right) / 2);
 
+  // increments our left to out of bounds which is higher of an index
+  // than right left <= right prevents this infinite loop
   while (arr[middle] !== value && left <= right) {
     // if value is smaller than middle, move the right pointer down
     if (value < arr[middle]) right = middle - 1;
