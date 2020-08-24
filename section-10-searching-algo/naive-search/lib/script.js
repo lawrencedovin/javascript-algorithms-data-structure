f3780d997fa78fcef242638c8441c7558049f90d
lawrencedovin <lawrence@qwikwire.com>
2020-08-24 16:24:47 -0400

    Updated naive search notes explanation

--- section-10-searching-algo/naive-search/lib/script.js
+++ section-10-searching-algo/naive-search/lib/script.js
@@ -2,20 +2,26 @@ function naiveSearch(long, short) {
   let count = 0;
   for(let i = 0; i < long.length; i++) {
     for(let j = 0; j < short.length; j++) {
-      if(short[j] !== long[i+j]) break;
-      // if no match break out to outer loop to iterate
+      if(short[j] !== long[i+j]){
+        console.log("No character match breaking inner loop") 
+        break;
+        // chars don't match break loop to iterate i until match of characters again
+      } 
       if(j === short.length - 1) count++;
+      // if j's index reaches to the end match of word is found +1
+      console.log(`short[j]: ${j}, long[i]: ${i}, long[i+j]: ${i+j}`,);
     }
+    console.log(`long[i]: ${i}`);
   }
   return count;
 }
 
 console.log(naiveSearch("lorie loled", "lol"));
-// i: "l"
-// j: "l"
+// i+j: 0, "l" 
+// j: 0, "l"
 
-// i: "o"
-// j: "o"
+// i+j: 1, "o"
+// j: 1, "o"
 
-// i: "r"
-// j: "l" X
+// i+j: 2, "r"
+// j: 2, "l" X
