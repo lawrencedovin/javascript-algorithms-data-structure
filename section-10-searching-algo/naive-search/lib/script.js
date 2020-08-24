a09d1f8167d24eebd381a044ac374eb2ff7765f5
lawrencedovin <lawrence@qwikwire.com>
2020-08-24 16:27:35 -0400

    Updated naive search notes explanation

--- section-10-searching-algo/naive-search/lib/script.js
+++ section-10-searching-algo/naive-search/lib/script.js
@@ -25,3 +25,31 @@ console.log(naiveSearch("lorie loled", "lol"));
 
 // i+j: 2, "r"
 // j: 2, "l" X
+
+// short[j]: 0, long[i]: 0, long[i+j]: 0
+// short[j]: 1, long[i]: 0, long[i+j]: 1
+// No character match breaking inner loop
+// long[i]: 0
+// No character match breaking inner loop
+// long[i]: 1
+// No character match breaking inner loop
+// long[i]: 2
+// No character match breaking inner loop
+// long[i]: 3
+// No character match breaking inner loop
+// long[i]: 4
+// No character match breaking inner loop
+// long[i]: 5
+// short[j]: 0, long[i]: 6, long[i+j]: 6
+// short[j]: 1, long[i]: 6, long[i+j]: 7
+// short[j]: 2, long[i]: 6, long[i+j]: 8
+// long[i]: 6
+// No character match breaking inner loop
+// long[i]: 7
+// short[j]: 0, long[i]: 8, long[i+j]: 8
+// No character match breaking inner loop
+// long[i]: 8
+// No character match breaking inner loop
+// long[i]: 9
+// No character match breaking inner loop
+// long[i]: 10
